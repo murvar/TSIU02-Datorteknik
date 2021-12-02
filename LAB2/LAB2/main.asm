88d8c02807d5866e9f4ec6d9498193dfb7799610
Vincent Ahlström <vinah331@student.liu.se>
2021-12-02 11:02:19 +0100

    Morse fungerar
    Allt utom mellanslag klart

--- LAB2/LAB2/main.asm
+++ LAB2/LAB2/main.asm
@@ -7,23 +7,74 @@
 
 
 start:
-	;ldi		r16, low(RAMEND)
-	;out		SPL, r16
-	;ldi		r16, high(RAMEND)
-	;out		SPH, r16
-	.def	time = r16 ; Length of tone
-	Out		DDRB,r16
-	ldi		r16,$ff
-	push	r16
-	call	BEEP
-	pop		r16
-	call	WAIT
-	push	r16
-	call	NOBEEP
-	pop		r16
-	call	WAIT
-	jmp		start
+	ldi		r20, low(RAMEND)
+	out		SPL, r20
+	ldi		r20, high(RAMEND)
+	out		SPH, r20
 
+	.equ	time=20 ; Length of tone
+
+	ldi		ZL,LOW(MESSAGE*2)
+	ldi		ZH,HIGH(MESSAGE*2)
+	ldi		r20,0B00010000
+	Out		DDRB,r20
+	call	MORSE
+
+MORSE:
+	call	GET_CHAR
+	cpi		r16,$00 ; char = 0? Done
+	breq	EXIT
+	push	r16
+	push	ZL
+	push	ZH
+	call	LOOKUP
+	call	SEND
+	pop		ZH
+	pop		ZL
+	pop		r16
+	adiw	ZL,1
+	ldi		r17,(2*time)
+	call	WAIT
+	jmp		MORSE
+GET_CHAR:
+	lpm		r16,Z
+	ret
+SEND:
+	cpi		r16,0
+	breq	RETURN
+	call	GET_BIT
+	call	BEEP
+	brcs	LONG
+SHORT://Kort beep
+	ldi		r17,time
+	jmp		CONTINUE
+LONG://Långt beep
+	ldi		r17,(3*time)
+CONTINUE:
+	call	WAIT
+	call	NOBEEP
+	ldi		r17,time
+	call	WAIT
+	jmp		SEND
+
+GET_BIT:
+	rol		r16 ; r16x2
+	ret
+
+	//pusha Z-pekaren
+LOOKUP:
+	ldi		ZL,LOW(BTAB<<1)
+	ldi		ZH,HIGH(BTAB<<1)
+	subi	r16,$41 
+	add		ZL,r16
+	lpm		r16,Z
+	ret
+
+RETURN:
+	ret
+
+EXIT:
+	jmp		EXIT2
 
 BEEP:
 	sbi		PORTB,4
@@ -33,41 +84,18 @@ NOBEEP:
 	cbi		PORTB,4
 	ret
 
-MESSAGE:
-	.db "DATORTTEKNIK", $00
-
-DELAY:
-	adiw	r28,63
-D_3:
-	adiw	r26,$3f
-D_2:
-	adiw	r24,$3f
-D_1:
-	dec		r28
-	brne	D_1
-	dec		r26
-	brne	D_2
-	dec		r24
-	brne	D_3
+WAIT_LONG:
+	adiw	r24,1
+	brne	WAIT_LONG
 	ret
 
 WAIT:
-    push r29
-    ldi r29, $ff
+	call	WAIT_LONG
+	dec		r17
+	brne	WAIT
+	ret
 
-WAIT2:
-    push r30
-    push r31
-    ldi r30, $00
-    ldi r31, $E0
+EXIT2:
 
-WAIT3:
-    adiw r30, 1
-    brne WAIT3
-    ; ~0.1 sekunder
-    pop r31
-    pop r30
-    dec r29
-    brne WAIT2
-    pop r29
-    ret
+MESSAGE:.db		"DATORTTEKNIK", $00
+BTAB:.db		$60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
